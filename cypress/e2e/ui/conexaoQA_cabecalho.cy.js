084ec92085703706403c68f2c6617ce99bd83a72
Lafaiete Rodrigues Machado <lafaiete.machado@gmail.com>
2023-08-16 20:23:53 -0300

    não logado

--- cypress/e2e/ui/conexaoQA_cabecalho.cy.js
+++ cypress/e2e/ui/conexaoQA_cabecalho.cy.js
@@ -33,10 +33,60 @@ describe('cabeçalho da página Home', () => {
                 .should('have.attr', 'href', '/login')
                 .and('not.have.attr', 'target', '_blank')
         })
+
+        it('valida o cabeçalho utilizando Object', () => {
+            
+            const menus = [
+                { seletor: 'navbar-conexaoQA', link: '/' },
+                { seletor: 'navbar-QAs', link: '/perfis' },
+                { seletor: 'navbar-about', link: '/sobre' },
+                { seletor: 'navbar-register', link: '/cadastrar' },
+                { seletor: 'navbar-login', link: '/login' }
+            ]
+
+            menus.forEach(({ seletor, link }) => {
+
+                cy.getElement(seletor)
+                    .should('have.attr', 'href', link)
+                    .and('not.have.attr', 'target', '_blank')
+            })
+        })
+
+        ;[
+            { seletor: 'navbar-conexaoQA', link: '/', menu: 'Conexão QA' },
+            { seletor: 'navbar-QAs', link: '/perfis', menu: 'QAs' },
+            { seletor: 'navbar-about', link: '/sobre', menu: 'Sobre' },
+            { seletor: 'navbar-register', link: '/cadastrar', menu: 'Cadastrar' },
+            { seletor: 'navbar-login', link: '/login', menu: 'Login' }
+        ].forEach(({ seletor, link, menu }) => {
+            it(`valida o menu ${menu} - Teste Dinâmico`, () => {
+
+                cy.getElement(seletor)
+                    .should('have.attr', 'href', link)
+                    .and('not.have.attr', 'target', '_blank')
+            })
+        })
+
     })
 
     context('logado', () => {
 
+        before(() => {
+            cy.login(Cypress.env('email'), Cypress.env('password'))
+        })
+
+        beforeEach(() => {
+            cy.visit('/')
+        })
         
+        afterEach(() => {
+            Cypress.Cookies.defaults({
+                preserve: []
+            })
+        })
+
+        it.only('teste', () => {
+            cy.log('teste')
+        })
     })
 })
