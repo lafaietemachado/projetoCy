a53d4a86130d56a2c5a6dc715b86931c2c9677d1
Lafaiete Rodrigues Machado <lafaiete.machado@gmail.com>
2023-08-28 16:23:31 -0300

    Adicionando cy.intercept

--- cypress/e2e/ui/conexaoQA_login.cy.js
+++ cypress/e2e/ui/conexaoQA_login.cy.js
@@ -1,14 +1,26 @@
 describe('página de login', () => {
-    
+
     beforeEach(() => {
         cy.visit('/login')
     })
 
     it('faz o login válido', () => {
-        
+
+        cy.intercept('GET', '/api/profile/me')
+            .as('apiLogin')
+
         cy.getElement('login-email').type(Cypress.env('email'), { log: false })
+
         cy.getElement('login-password').type(Cypress.env('password'))
-        cy.getElement('login-submit').click()
+
+        // eslint-disable-next-line cypress/unsafe-to-chain-command
+        cy.getElement('login-submit')
+            .click()
+            .wait('@apiLogin')
+            .then(({ response }) => {
+                expect(response.body.bio).to.eq('QA')
+            })
+
         cy.getElement('dashboard-welcome').should('contain', 'Lafaiete')
     })
 })
